212649d8d4d419fec6b86f38b017b7a30f5d6f2c
Gerardo Itzcoatl <itzcoatl3b@gmail.com>
2021-04-02 16:41:28 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  StyleSheet,
+  Text,
+
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  
+
+  return (
+   <>
+   </>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
